2db3085281469ac1a4d41e1be198dff76b39581e
liuruijun <ruijun.liu-a1654@aqara.com>
2021-09-29 10:01:16 +0800

    init vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>hello world</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="less"></style>
