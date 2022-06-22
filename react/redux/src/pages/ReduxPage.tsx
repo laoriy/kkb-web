53b024400f1c75336666444d3913c60ab85be0b8
laor <2680406465@qq.com>
2022-06-22 10:20:43 +0800

    feat: redux base finish

--- react/redux/src/pages/ReduxPage.tsx
+++ react/redux/src/pages/ReduxPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import store from "../store/";
+import store from "../store";
+import { countAdd, countMinus } from "../store/slices/count";
 
 const ReduxPage = () => {
   const [_ignored, forceUpdate] = React.useReducer((x) => x + 1, 0);
@@ -9,12 +10,35 @@ const ReduxPage = () => {
       forceUpdate();
     });
   }, []);
+
+  const asyncAdd = () => {
+    //@ts-ignore
+    store.dispatch((dispatch: any) => {
+      setTimeout(() => {
+        dispatch({ type: "ADD" });
+      }, 1000);
+    });
+  };
+  const asyncAdd2 = () => {
+    //@ts-ignore
+    store.dispatch((dispatch) => {
+      setTimeout(() => {
+        dispatch(countAdd());
+      }, 1000);
+    });
+  };
   return (
     <div>
       <h3>ReduxPage</h3>
       <p>{store.getState()}</p>
       <button onClick={() => store.dispatch({ type: "ADD" })}>add</button>
       <button onClick={() => store.dispatch({ type: "MINUS" })}>minus</button>
+      <button onClick={asyncAdd}>asyncAdd</button>
+      <h3>ReduxTooltip</h3>
+      {/* <p>{store.getState().countReducer}</p> */}
+      <button onClick={() => store.dispatch(countAdd())}>add</button>
+      <button onClick={() => store.dispatch(countMinus())}>minus</button>
+      <button onClick={asyncAdd2}>asyncAdd2</button>
     </div>
   );
 };
--- react/redux/src/store/index.ts
+++ react/redux/src/store/index.ts
@@ -1,17 +1,6 @@
-import { createStore, Action } from "redux";
-
-//定义state初始化和修改规则,reducer是一个纯函数
-function counterReducer(state = 0, action: Action) {
-  switch (action.type) {
-    case "ADD":
-      return state + 1;
-    case "MINUS":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-const store = createStore(counterReducer);
+import store from "./redux";
+// import store from "./redux-toolkit";
+// as Store<number, Action<any>>
 export interface RootState {
   num: number;
 }
