8bfda048f3e603d19fa83b58f9b1e1e40d21df59
laor <2680406465@qq.com>
2022-06-27 09:54:17 +0800

    feat: redex mapState

--- react/react-redux/src/pages/ReactReduxPage.tsx
+++ react/react-redux/src/pages/ReactReduxPage.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import { connect, ConnectedProps, useDispatch, useSelector } from "react-redux";
+import { bindActionCreators, Dispatch } from "redux";
 import { RootState } from "../store";
 import { countAdd, countMinus } from "../store/slices/count";
 interface BaseProps {
@@ -13,26 +14,50 @@ interface BaseProps {
  */
 const mapState = (state: RootState, ownProps: BaseProps) => {
   //   console.log(ownProps, "ownProps"); // 只要指定了ownProps ，不管有没有使用，props变化都会进行调用mapState函数
-  console.log("ff");
+  console.log("execute mapState--");
 
   return {
     num: state,
   };
 };
 
-const mapDispatch = () => {
-  // 只要props 变化了就会调用mapDispatch，即使没有指定ownProps
-  return {
+// const mapDispatchToProps = (dispatch: Dispatch, ownProps: BaseProps) => {
+//   //  // 只要指定了ownProps ，不管有没有使用，props变化都会进行调用mapState函数
+//   console.log("execute mapDispatch~");
+
+//   return {
+//     add: () => dispatch({ type: "ADD" }),
+//     minus: () => dispatch({ type: "MINUS" }),
+//   };
+// };
+
+/**
+ * 上面手动加dispatch也可以通过bindActionCreators进行处理
+ */
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  let res = {
     add: () => ({ type: "ADD" }),
     minus: () => ({ type: "MINUS" }),
   };
+  res = bindActionCreators(res, dispatch);
+  return {
+    ...res,
+  };
 };
-
-const connector = connect(mapState, mapDispatch);
+// const mapDispatchToProps = {
+//   add: () => ({ type: "ADD" }),
+//   minus: () => ({ type: "MINUS" }),
+// };
+/**
+ * 如果省略mapDispatchToProps这个参数，则dispatch会注入到props中。
+ */
+const connector = connect(mapState, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 interface Props extends PropsFromRedux, BaseProps {}
 const ReactReduxPage = (props: Props) => {
+  console.log(props);
+
   const { add, num, minus, color } = props;
 
   return (
