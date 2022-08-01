ccd1e509c07ad1103c71f98269a0f3774be6a88d
laor <2680406465@qq.com>
2022-08-01 22:23:16 +0800

    feat:controller

--- node/lesson/07-eggjs/lgg/laor-loader.js
+++ node/lesson/07-eggjs/lgg/laor-loader.js
@@ -23,24 +23,54 @@ function load(dir, cb) {
 }
 
 
-function initRouter() {
+function initRouter(app) {
     const router = new Router()
 
     load('routes', (filename, routes) => {
         // 前缀
         const prefix = filename === 'index' ? '' : `/${filename}`
-
+        routes = typeof routes === 'function' ? routes(app) : routes
         // 遍历对象
         Object.keys(routes).forEach(key => {
             const [method, path] = key.split(' ')
 
             console.log(`正在映射地址：${method.toUpperCase()} ${prefix}${path}`);
-            router[method](prefix + path, routes[key])
+            router[method](prefix + path, async ctx => {
+                // 挂载上下文到APP
+                app.ctx = ctx
+                // 路由处理接收上下文
+                await routes[key](app)
+            })
+            // router[method](prefix + path, routes[key])
 
         })
 
     })
-   return router
+    return router
 }
 
-module.exports = { initRouter }
+
+function initController(app) {
+    const controllers = {}
+
+    // 读取ctrl
+
+    load('controller', (filename, controller) => {
+        controllers[filename] = controller(app)
+    })
+
+    return controllers
+}
+
+
+function initService(app) {
+    const services = {}
+
+    load('service', (filename, service) => {
+        services[filename] = service
+    })
+
+    return services
+}
+
+module.exports = { initRouter, initController, initService }
--- node/lesson/07-eggjs/lgg/routes/index.js
+++ node/lesson/07-eggjs/lgg/routes/index.js
@@ -1,8 +1,10 @@
-module.exports = {
-    'get /': async ctx => {
-        ctx.body = '⾸⻚'
-    },
-    'get /detail': ctx => {
-        ctx.body = '详情⻚⾯'
-    }
-}
+module.exports = app => ({
+    // 'get /': async ctx => {
+    //     ctx.body = '⾸⻚'
+    // },
+    // 'get /detail': ctx => {
+    //     ctx.body = '详情⻚⾯'
+    // }
+    'get /': app.$ctrl.home.index,
+    'get /detail': app.$ctrl.home.detail
+})
